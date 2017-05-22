619d8e84a40d5e07f8e350de55700670535907b9
Antonio Ibáñez <antonio@winvestify.com>
2017-05-22 18:57:22 +0200

    2017/05/22 Added parallelization to Comunitae.

--- companyCodeFiles/comunitae.php
+++ companyCodeFiles/comunitae.php
@@ -65,6 +65,10 @@ if subscriptionProgress = "finalizado" then write -1 in DB field
 
 
 class comunitae extends p2pCompany{
+    
+    private $numberOfPages;
+    private $index;
+    private $random;
 
 		
 function __construct() {
@@ -201,6 +205,302 @@ function calculateLoanCost($amount, $duration, $interestRate)  {
         }
         return $totalArray;
     }
+    
+    /**
+     *
+     * 	Collects the investment data of the user
+     * 	@return array	Data of each investment of the user as an element of an array
+     * 	
+     */
+    function collectUserInvestmentData($str) {
+// user = "antoine@winvestify.com"
+// pw = "Zastac2015"
+// manoloherrero@msn.com    Mecano1980E
+//$configurationParameters = array('appDebug' => true);
+//$this->defineConfigParms($configurationParameters);
+
+        switch ($this->idForSwitch) {
+            case 0:
+                $credentials['j_username'] = $this->user;
+                $credentials['j_password'] = $this->password;
+                $this->idForSwitch++;
+                $this->doCompanyLogin($credentials);
+                break;
+            
+            case 1:
+                // Check if user actually has entered the portal of the company.
+                // by means of checking of 2 unique identifiers of the portal
+                // This should be done by checking a field in the Webpage (button, link etc)
+                // and the email of the user (if aplicable)
+                $dom = new DOMDocument;
+                libxml_use_internal_errors(true); //Use it when there is an error on the HTML coming from cURL
+                $dom->loadHTML($str);
+                $dom->preserveWhiteSpace = false; 
+
+                $confirm = 0;
+                $uls = $dom->getElementsByTagName('ul');
+                $this->verifyNodeHasElements($uls);
+                if (!$this->hasElements) {
+                    return $this->getError(__LINE__, __FILE__);
+                }
+                foreach ($uls as $ul) {
+                    $as = $ul->getElementsByTagName('a');
+                    $this->verifyNodeHasElements($as);
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    foreach ($as as $a) {
+                        if (strcasecmp(trim($a->nodeValue), trim($this->user)) == 0) {
+                            $confirm++;
+                            break 2;
+                        }
+                    }
+                }
+
+                $as = $dom->getElementsByTagName('a');
+                $this->verifyNodeHasElements($as);
+                if (!$this->hasElements) {
+                    return $this->getError(__LINE__, __FILE__);
+                }
+                foreach ($as as $a) {
+                    if (strncasecmp(trim($a->getAttribute('href')), "/mi-posicion", 12) === 0) {
+                        $confirm++;
+                        break;
+                    }
+                }
+                $resultComunitae = 0;
+                if ($confirm > 0) {
+                    $this->mainPortalPage = $str;
+                    $resultComunitae = 1;
+                }
+                if (!$resultComunitae) {   // Error while logging in
+                    echo __FILE__ . " " . __LINE__ . "ERROR WHILE LOGGING IN<br>";
+                    $tracings = "Tracing:\n";
+                    $tracings .= __FILE__ . " " . __LINE__ . "ERROR WHILE LOGGING IN\n";
+                    $tracings .= "Comunitae login: userName =  " . $this->config['company_username'] . ", password = " . $this->config['company_password'] . " \n";
+                    $tracings .= " \n";
+                    $msg = "Error while logging in user's portal. Wrong userid/password \n";
+                    $msg = $msg . $tracings . " \n";
+                    $this->logToFile("Warning", $msg);
+                    exit;
+                }
+                echo __FILE__ . " " . __LINE__ . " LOGIN CONFIRMED<br>";
+                $dom = new DOMDocument;
+                $dom->loadHTML($this->mainPortalPage); // obtained in the function	"companyUserLogin"	
+                $dom->preserveWhiteSpace = false;
+
+                echo __FILE__ . " " . __LINE__ . "<br>";
+                $this->idForSwitch++;
+                $this->getCompanyWebpage();  // load Webpage into a string variable so it can be parsed
+                break;
+
+            case 2:
+                //echo $str;
+                $this->random = rand(111, 900);
+                $url = array_shift($this->urlSequence);
+                $this->idForSwitch++;
+                $this->getCompanyWebpage($url . time() . $this->random); // https://www.comunitae.com/pastillaCuenta.html?aleat=1487321358524	
+                break;
+            case 3:
+                echo __FILE__ . " " . __LINE__ . "<br>";
+                //echo $str;
+                $dom = new DOMDocument;
+                libxml_use_internal_errors(true);
+                $dom->loadHTML($str);
+                $dom->preserveWhiteSpace = false;
+
+                $accountSummarys = $this->getElements($dom, "div", "id", "pastillaCuenta_10");
+                if (!$this->hasElements) {
+                    return $this->getError(__LINE__, __FILE__);
+                }
+                $items = $this->getElements($accountSummarys[0], "span", "class", "list-group-item");
+                if (!$this->hasElements) {
+                    return $this->getError(__LINE__, __FILE__);
+                }
+                $spans = $this->getElements($items[2], "span", "class", "badge");
+                if (!$this->hasElements) {
+                    return $this->getError(__LINE__, __FILE__);
+                }
+                $this->tempArray['global']['myWallet'] = $this->getMonetaryValue($spans[0]->nodeValue);
+
+                $this->random++;
+                $url = array_shift($this->urlSequence);
+                $this->idForSwitch++;
+                $this->getCompanyWebpage($url . time() . $this->random); // https://www.comunitae.com/www.comunitae.com/mi-cartera/pagares
+                break;
+            case 4:
+                $url = array_shift($this->urlSequence);
+                $this->idForSwitch++;
+                $this->getCompanyWebpage($url);   // https://www.comunitae.com/listarParticipaciones.html?method=mostrarBloques&_=1487322997347 
+                break;
+            
+            case 5:
+                //echo $str;
+                $url = array_shift($this->urlSequence);  // https://www.comunitae.com/listarParticipaciones.html?method=irPestanaListado&id=1&PARAM_PAGINACION_PAGINA_ACTUAL=
+                $this->idForSwitch++;
+                $this->getCompanyWebpage($url . "1");
+                break;
+            case 6:
+                //echo "KKKKKKKKK" . $str;
+                $domAccount = new DOMDocument;
+                libxml_use_internal_errors(true);
+                $domAccount->loadHTML($str);
+                $domAccount->preserveWhiteSpace = false;
+                $accountSummaries = $this->getElements($domAccount, "article", "class", "row panel-row prestamo");
+                if (!$this->hasElements) {
+                    return $this->getError(__LINE__, __FILE__);
+                }
+                $this->index = -1;
+                foreach ($accountSummaries as $key => $account) {
+                    $investmentInfos = $this->getElements($account, "p", "class", "form-control-static");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $as = $this->getElements($investmentInfos[0], "a");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $tempStatus = $this->getLoanState($as[0]->getAttribute("title")); // status of actual investment
+                    if ($tempStatus == TERMINATED_OK) {
+                        continue;         // skip this one as investment has finished
+                    }
+
+                    $this->index++;
+                    $this->data1[$this->index]['status'] = $tempStatus; // status of actual investment
+                    $this->data1[$this->index]['loanId'] = trim($investmentInfos[1]->nodeValue);
+                    $this->data1[$this->index]['name'] = trim($investmentInfos[2]->nodeValue);
+                    $tempData = explode("-", $investmentInfos[4]->nodeValue);
+                    $this->data1[$this->index]['date'] = $this->getSpanishMonthNumber(trim($tempData[0])) . "-" . trim($tempData[1]);
+                    $as = $this->getElements($investmentInfos[3], "a");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $this->data1[$this->index]['duration'] = filter_var($as[2]->nodeValue, FILTER_SANITIZE_NUMBER_INT) . " D&iacute;as";
+                    $this->data1[$this->index]['interest'] = $this->getPercentage($as[1]->nodeValue);
+                    $as = $this->getElements($investmentInfos[4], "a");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $spans = $this->getElements($investmentInfos[5], "span");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $this->data1[$this->index]['invested'] = $this->getMonetaryValue($spans[0]->nodeValue);
+                    $as = $this->getElements($investmentInfos[0], "a");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $this->data1[$this->index]['status'] = $this->getLoanState($as[0]->getAttribute("title")); // status of actual investment
+                    $this->tempArray['global']['totalEarnedInterest'] = $this->tempArray['global']['totalEarnedInterest'] +
+                            $this->data1[$key]['profitGained'];
+                    $this->tempArray['global']['totalAmortized'] = $this->tempArray['global']['totalAmortized'] + $this->data1[$this->index]['amortized'];
+                    $this->tempArray['global']['totalInvestment'] = $this->tempArray['global']['totalInvestment'] + $this->data1[$this->index]['invested'];
+                    $this->tempArray['global']['totalPercentage'] = $this->tempArray['global']['totalPercentage'] + $this->data1[$this->index]['interest'];
+                }
+
+                $this->tempArray['global']['activeInInvestments'] = $this->tempArray['global']['totalInvested'] - $this->tempArray['global']['totalAmortized'];
+                $this->print_r2($this->data1);
+        // Check number of pages. It seems the investments are shown in pages of 15 at the time.
+                $pages = $this->getElements($domAccount, "ul", "class", "pagination no-margin");
+                echo __FILE__ . " " . __LINE__ . "<br>";
+                if (empty($pages)) {
+                    $this->numberOfPages = 1;
+                } else {
+                    $as = $this->getElements($pages[0], "a");
+                    $this->numberOfPages = count($as);
+                }
+                echo __FILE__ . " " . __LINE__ . "number of pages = $this->numberOfPages<br>";
+                $numberOfPages = 0;
+                if ($this->numberOfPages > 1) {
+                    for ($i = 2; $i <= $this->numberOfPages; $i++) {
+                        $this->tempUrl[$numberOfPages] = $url . $i; // https://www.comunitae.com/listarParticipaciones.html?method=irPestanaListado&id=2&PARAM_PAGINACION_PAGINA_ACTUAL=
+                        $numberOfPages++;
+                    }
+                    $this->numberOfInvestments = $this->numberOfPages - 2;
+                    $this->idForSwitch++;
+                }
+                else {
+                    $this->tempArray['global']['activeInInvestments'] = $this->tempArray['global']['totalInvestment'] - $this->tempArray['global']['totalAmortized'];
+                    $this->tempArray['global']['profitibility'] = (int) ($this->tempArray['global']['totalPercentage'] / ($this->tempArray['global']['investments'] = $this->index + 1));
+                    $this->print_r2($this->tempArray);
+                    $this->tempArray['global']['investments'] = $this->index++;
+                    
+                    $this->tempArray['investments'] = $this->data1;
+                    //	$tempArray['global']['activeInInvestments'] = $tempArray['global']['activeInInvestments'] + $data1[$key]['amortized'];
+                    //	$tempArray['global']['totalInvested'] = $tempArray['global']['totalInvested'] + $data1[$key]['invested'];
+                    return $this->tempArray;
+                }
+            case 7:
+                $this->idForSwitch++;
+                $this->getCompanyWebpage($this->tempUrl[$this->accountPosition]);	// https://www.comunitae.com/listarParticipaciones.html?method=irPestanaListado&id=2&PARAM_PAGINACION_PAGINA_ACTUAL=
+                break;
+            case 8:
+                $domAccount = new DOMDocument;
+                libxml_use_internal_errors(true);
+                $domAccount->loadHTML($str);
+                $domAccount->preserveWhiteSpace = false;
+                $accountSummaries = $this->getElements($domAccount, "article", "class", "row panel-row prestamo");
+                if (!$this->hasElements) {
+                    return $this->getError(__LINE__, __FILE__);
+                }
+                foreach ($accountSummaries as $key => $account) {
+                    $this->index++;
+                    $investmentInfos = $this->getElements($account, "p", "class", "form-control-static");
+                    //			foreach ($investmentInfos as $summary){
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $this->data1[$this->index]['loanId'] = trim($investmentInfos[1]->nodeValue);
+                    $this->data1[$this->index]['name'] = trim($investmentInfos[2]->nodeValue);
+                    $tempData = explode("-", $investmentInfos[4]->nodeValue);
+                    $this->data1[$this->index]['date'] = $this->getSpanishMonthNumber(trim($tempData[0])) . "-" . trim($tempData[1]);
+                    $as = $this->getElements($investmentInfos[3], "a");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $this->data1[$this->index]['duration'] = filter_var($as[2]->nodeValue, FILTER_SANITIZE_NUMBER_INT) . " D&iacute;as";
+                    $this->data1[$this->index]['interest'] = $this->getPercentage($as[1]->nodeValue);
+                    $as = $this->getElements($investmentInfos[4], "a");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $spans = $this->getElements($investmentInfos[5], "span");
+
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $this->data1[$this->index]['invested'] = $this->getMonetaryValue($spans[0]->nodeValue);
+                    $as = $this->getElements($investmentInfos[0], "a");
+                    if (!$this->hasElements) {
+                        return $this->getError(__LINE__, __FILE__);
+                    }
+                    $this->data1[$this->index]['status'] = $this->getLoanState($as[0]->getAttribute("title")); // status of actual investment
+                    //			}
+                    $this->tempArray['global']['totalEarnedInterest'] = $this->tempArray['global']['totalEarnedInterest'] +
+                            $this->data1[$key]['profitGained'];
+                    $this->tempArray['global']['totalAmortized'] = $this->tempArray['global']['totalAmortized'] + $this->data1[$this->index]['amortized'];
+                    $this->tempArray['global']['totalInvestment'] = $this->tempArray['global']['totalInvestment'] + $this->data1[$this->index]['invested'];
+                    $this->tempArray['global']['totalPercentage'] = $this->tempArray['global']['totalPercentage'] + $this->data1[$this->index]['interest'];
+                }
+                if ($this->numberOfPages-1 != $this->accountPosition) {
+                    $this->idForSwitch = 7;
+                    $this->accountPosition++;
+                    $this->getCompanyWebpage($this->tempUrl[$this->accountPosition]);     // Load amortization Table
+                    break;
+                }
+                else {
+                    $this->tempArray['global']['activeInInvestments'] = $this->tempArray['global']['totalInvestment'] - $this->tempArray['global']['totalAmortized'];
+                    $this->tempArray['global']['profitibility'] = (int) ($this->tempArray['global']['totalPercentage'] / ($this->tempArray['global']['investments'] = $this->index + 1));
+                    $this->print_r2($this->tempArray);
+                    $this->tempArray['global']['investments'] = $this->index++;
+                    $this->tempArray['investments'] = $this->data1;
+                    //	$tempArray['global']['activeInInvestments'] = $tempArray['global']['activeInInvestments'] + $data1[$key]['amortized'];
+                    //	$tempArray['global']['totalInvested'] = $tempArray['global']['totalInvested'] + $data1[$key]['invested'];
+                    return $this->tempArray;
+                }
+                break;
+        }
+    }
 
     /**
 *
@@ -208,7 +508,7 @@ function calculateLoanCost($amount, $duration, $interestRate)  {
 *	@return array	Data of each investment of the user as an element of an array
 *	NOT READY
 */
-function collectUserInvestmentData($user, $password) {
+function collectUserInvestmentDataSequencial($user, $password) {
 // user = "antoine@winvestify.com"
 // pw = "Zastac2015"
 // manoloherrero@msn.com    Mecano1980E
