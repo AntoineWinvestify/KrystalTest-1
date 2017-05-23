b6ee37850d4c9c3fc483d7ae88d8882a8e76529a
Antonio Ibáñez <antonio@winvestify.com>
2017-05-23 14:19:46 +0200

    2017/05/23 |Antonio| Commented two lines because are unnecessary

--- companyCodeFiles/arboribus.php
+++ companyCodeFiles/arboribus.php
@@ -216,8 +216,8 @@ class arboribus extends p2pCompany {
                 foreach ($as as $a) {
                     if (strcasecmp(trim($a->nodeValue), "Salir") == 0) {  // Login confirmed
                         $this->mainPortalPage = $str;
-                        array_shift($this->urlSequence);
-                        array_shift($this->urlSequence);
+                        //array_shift($this->urlSequence);
+                        //array_shift($this->urlSequence);
                         $resultMyArboribus = true;
                     }
                 }
