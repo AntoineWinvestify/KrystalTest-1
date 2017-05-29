774da7eac88cd4f33e658a5551b621bf8e38aa92
eduardo <eduardo@Frodo>
2017-05-29 10:26:53 +0200

    29/05/2017 | Eduardo  | Ocr  model for ocr
      function ocrDataSave   Save ocr data in db                         [OK]
      function ocrGetData    Get info of ocr                             [OK]

---
+++ Model/Ocr.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * +--------------------------------------------------------------------------------------------+
+ * | Copyright (C) 2016, http://www.winvestify.com                   	  	|
+ * +--------------------------------------------------------------------------------------------+
+ * | This file is free software; you can redistribute it and/or modify 		|
+ * | it under the terms of the GNU General Public License as published by  |
+ * | the Free Software Foundation; either version 2 of the License, or 	|
+ * | (at your option) any later version.                                      		|
+ * | This file is distributed in the hope that it will be useful   		    	|
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of    		|
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the      	|
+ * | GNU General Public License for more details.        			              	|
+ * +---------------------------------------------------------------------------------------------------------------+
+ *
+ *
+ * @author
+ * @version 0.1
+ * @date 2016-10-25
+ * @package
+ *
+
+  2016/29/2017 version 0.1
+  function ocrDataSave   Save ocr data in db                         [OK]
+  function ocrGetData    Get info of ocr                             [OK]
+
+
+ */
+App::uses('CakeEvent', 'Event');
+
+class ocr extends AppModel {
+
+    var $name = 'Ocr';
+    var $validate = array();
+    public $hasMany = array(
+        'Comapany' => array(
+            'className' => 'Comapany',
+            'foreignKey' => 'ocr_id',
+            'fields' => '',
+            'order' => '',
+        ),
+    );
+
+    /*
+     *
+     * Saves data in ocr table
+     * 
+     */
+
+    public function ocrDataSave($datos) {
+
+        echo '1';
+        print_r('id = ' . $datos['Investor_id']);
+        $id = $this->find('first', array(
+            'fields' => array(
+                'id',
+            ),
+            'conditions' => array(
+                'Investor_id' => $datos['Investor_id']),
+            'recursive' => -1,));
+        print_r($id);
+        //Si ya existe, actualizo esa fila del ocr
+        if (count($id) > 0) {
+
+            echo 'existe';
+            if ($datos['Ocr_vehicle'] == 1) {
+                $data = array(
+                    'id' => $id['Ocr']['id'],
+                    'Investor_id' => $datos['Investor_id'],
+                    'Ocr_vehicle' => 1,
+                    'Investor_cif' => $datos['Investor_cif'],
+                    'Investor_businessName' => $datos['Investor_businessName'],
+                    'Investor_iban' => $datos['Investor_iban'],
+                );
+            } else {
+                $data = array(
+                    'id' => $id['Ocr']['id'],
+                    'Investor_id' => $datos['Investor_id'],
+                    'Ocr_vehicle' => 0,
+                    'Investor_cif' => null,
+                    'Investor_businessName' => null,
+                    'Investor_iban' => $datos['Investor_iban'],
+                );
+            }
+        //Si no existe, creo una nueva fila ocr    
+        } else {
+            echo 'no existe';
+            if ($datos['Ocr_vehicle'] == 1) {
+                $data = array(  
+                    'Investor_id' => $datos['Investor_id'],
+                    'Ocr_vehicle' => 1,
+                    'Investor_cif' => $datos['Investor_cif'],
+                    'Investor_businessName' => $datos['Investor_businessName'],
+                    'Investor_iban' => $datos['Investor_iban'],
+                );
+            } else {
+                $data = array(
+                    'Investor_id' => $datos['Investor_id'],
+                    'Ocr_vehicle' => 0,
+                    'Investor_cif' => null,
+                    'Investor_businessName' => null,
+                    'Investor_iban' => $datos['Investor_iban'],
+                );
+            }
+        }
+
+
+
+        print_r($data);
+
+
+        $this->save($data);
+        $result[0] = 1;
+        //Insert OK
+        return $result;
+    }
+
+    /*
+     * 
+     * Get data of ocr table
+     * 
+     */
+
+    public function ocrGetData($id) {
+
+        $info = $this->find("all", array(
+            'conditions' => array('Investor_id' => $id),
+            'recursive' => -1,
+        ));
+        return $info;
+    }
+
+    public function saveDocuments() {
+        
+    }
+
+}
