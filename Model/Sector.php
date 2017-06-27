7f95ab70b899aab2903963b9d0549ea22fe72559
Antonio Ibáñez <antonio@winvestify.com>
2017-06-27 09:51:25 +0200

    2017/06/27 | Antonio-Admin | Finished sector model and relationship wit role model

--- Model/Sector.php
+++ Model/Sector.php
@@ -34,60 +34,9 @@ class Sector extends AppModel {
                 'className' => 'Role',
                 'joinTable' => 'roles_sectors',
                 'foreignKey' => 'sector_id',
-                'associationForeignKey' => 'role_id',
-                'unique' => true,
-                'conditions' => '',
-                'fields' => '',
-                'order' => '',
-                'limit' => '',
-                'offset' => '',
-                'finderQuery' => '',
-                'with' => ''
+                'associationForeignKey' => 'role_id'
             )
     );
     
-    /*public $hasMany = array(
-			'Linkedaccount' => array(
-				'className' => 'Linkedaccount',
-				'foreignKey' => 'investor_id',
-				'fields' => '',
-				'order' => '',
-				),
-			);*/
-
-    /*public $hasOne = array (
-				'User' => array(
-				'className' => 'User',
-				'foreignKey' => 'investor_id',
-				'fields' => '',
-				'order' => '',
-				),
-			);*/
     
-    /**
-    *
-    *	Translates the unique userReference to the database reference
-    *	@param 		string	$role It is the role that user has in the website
-    * 	@return 	int	$investorId The database reference of the investor
-    * 					
-    */
-    function getSectorsByRole($roleId = null){
-        if (empty($roleId)) {
-            return false;
-        }
-        $sectors = $this->find('all', array(
-            'joins' => array(
-                 array('table' => 'roles_sectors',
-                    //'alias' => 'KitchensRestaurant',
-                    'type' => 'INNER',
-                    'conditions' => array(
-                        'roles_sectors.role_id' => $roleId,
-                        'roles_sectors.sector_id = sectors.id'
-                    )
-                )
-            ),
-            'group' => 'sectors.id'
-        ));
-        return $sectors;
-    }
 }
