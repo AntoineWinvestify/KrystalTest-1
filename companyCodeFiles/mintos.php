8d8d0a3c47de67fb99314d809cacbd1d5f40d916
Antonio Ibáñez <antonio@winvestify.com>
2017-08-16 16:08:21 +0200

    2017/08/16 | Antonio-Dashboard | Created file mintos

---
+++ companyCodeFiles/mintos.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * +-----------------------------------------------------------------------------+
+ * | Copyright (C) 2017, http://www.winvestify.com                   	  	|
+ * +-----------------------------------------------------------------------------+
+ * | This file is free software; you can redistribute it and/or modify 		|
+ * | it under the terms of the GNU General Public License as published by  	|
+ * | the Free Software Foundation; either version 2 of the License, or 		|
+ * | (at your option) any later version.                                      	|
+ * | This file is distributed in the hope that it will be useful   		|
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of    		|
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                |
+ * | GNU General Public License for more details.        			|
+ * +-----------------------------------------------------------------------------+
+ *
+ *
+ * Contains the code required for accessing the website of "Mintos"
+ *
+ * 
+ * @author 
+ * @version 0.1
+ * @date 2017-08-16
+ * @package  
+
+  /**
+ * Description of mintos
+ *
+ */
+
+class mintos extends p2pCompany {
+    function __construct() {
+	parent::__construct();	
+        // Do whatever is needed for this subsclass
+
+    }
+}
