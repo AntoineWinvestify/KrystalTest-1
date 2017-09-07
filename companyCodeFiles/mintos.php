80b5ea788fd24fbc2c26a774e10c83ee231c8ed1
eduardo <eduardo@Frodo>
2017-09-07 11:11:23 +0200

    2017-09-07 | Eduardo | Dashboard | Added function to get file on Mintos

--- companyCodeFiles/mintos.php
+++ companyCodeFiles/mintos.php
@@ -148,8 +148,8 @@ class mintos extends p2pCompany {
                 $input = $this->getElements($dom, 'input', 'name', '_csrf_token');
                 $csrf = $input[0]->getAttribute('value'); //this is the csrf token
 
-                $this->credentials['username'] = $this->user;
-                $this->credentials['password'] = $this->password;
+                $this->credentials['_username'] = $this->user;
+                $this->credentials['_password'] = $this->password;
                 $this->credentials['_csrf_token'] = $csrf;
                 $this->credentials['_submit'] = '';
 
@@ -163,7 +163,7 @@ class mintos extends p2pCompany {
             case 2:
                 echo $this->idForSwitch . HTML_ENDOFLINE;
                 $this->idForSwitch++;
-                echo $str;
+                //echo $str;
                 $next = $this->getCompanyWebpageMultiCurl();
                 echo 'Next: ' . $next . HTML_ENDOFLINE;
                 break;
@@ -205,7 +205,7 @@ class mintos extends p2pCompany {
             case 4:
                 echo $this->idForSwitch . HTML_ENDOFLINE;         
                 echo 'Login ok';
-                $fileUrl = $str;
+                $fileUrl = array_shift($this->urlSequence);
                 echo $fileUrl . HTML_ENDOFLINE;
                 $credentialsFile = 'purchased_from=&purchased_till=&statuses%5B%5D=256&statuses%5B%5D=512&statuses%5B%5D=1024&statuses%5B%5D=2048&statuses%5B%5D=8192&statuses%5B%5D=16384&+=256&+=512&+=1024&+=2048&+=8192&+=16384&listed_for_sale_status=&min_interest=&max_interest=&min_term=&max_term=&with_buyback=&min_ltv=&max_ltv=&loan_id=&sort_field=&sort_order=DESC&max_results=20&page=1&include_manual_investments=';
                 $fileName = 'Investment';
