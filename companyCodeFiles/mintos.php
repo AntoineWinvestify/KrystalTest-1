4597db90e7cf338cb16386c2291dc9efec09ac60
Antonio Ibáñez <antonio@winvestify.com>
2017-09-18 17:23:05 +0200

    2017/09/18 | Antonio-Dashboard | Adding variables correctly to download files on Mintos

--- companyCodeFiles/mintos.php
+++ companyCodeFiles/mintos.php
@@ -322,11 +322,11 @@ class mintos extends p2pCompany {
                 break;
             ////////DOWNLOAD FILE
             case 4:
-                $credentialsFile = 'purchased_from=&purchased_till=&statuses%5B%5D=256&statuses%5B%5D=512&statuses%5B%5D=1024&statuses%5B%5D=2048&statuses%5B%5D=8192&statuses%5B%5D=16384&+=256&+=512&+=1024&+=2048&+=8192&+=16384&listed_for_sale_status=&min_interest=&max_interest=&min_term=&max_term=&with_buyback=&min_ltv=&max_ltv=&loan_id=&sort_field=&sort_order=DESC&max_results=20&page=1&include_manual_investments=';
+                //$credentialsFile = 'purchased_from=&purchased_till=&statuses%5B%5D=256&statuses%5B%5D=512&statuses%5B%5D=1024&statuses%5B%5D=2048&statuses%5B%5D=8192&statuses%5B%5D=16384&+=256&+=512&+=1024&+=2048&+=8192&+=16384&listed_for_sale_status=&min_interest=&max_interest=&min_term=&max_term=&with_buyback=&min_ltv=&max_ltv=&loan_id=&sort_field=&sort_order=DESC&max_results=20&page=1&include_manual_investments=';
                 $fileName = 'Investment';
-                $referer = 'https://www.mintos.com/en/my-investments/?currency=978&statuses[]=256&statuses[]=512&statuses[]=1024&statuses[]=2048&statuses[]=8192&statuses[]=16384&sort_order=DESC&max_results=20&page=1';
+                //$referer = 'https://www.mintos.com/en/my-investments/?currency=978&statuses[]=256&statuses[]=512&statuses[]=1024&statuses[]=2048&statuses[]=8192&statuses[]=16384&sort_order=DESC&max_results=20&page=1';
                 $this->idForSwitch++;
-                $this->getPfpFileMulticurl(null, $fileName, 'Mintos', 'prueba', $credentialsFile,$referer);
+                $this->getPfpFileMulticurl(null, null, null, $fileName);
                 //echo 'Downloaded';
                 break;
             case 5:
@@ -334,11 +334,17 @@ class mintos extends p2pCompany {
                 $this->getCompanyWebpageMultiCurl();
                 break;
             case 6:
-                $credentialsFile = "account_statement_filter[fromDate]=12.09.2017&account_statement_filter[toDate]=12.09.2017&account_statement_filter[maxResults]=20";
+                $today = date("d.m.y");  
+                //$credentialsFile = "account_statement_filter[fromDate]={$today}&account_statement_filter[toDate]={$today}&account_statement_filter[maxResults]=20";
+                $url = array_shift($this->urlSequence);
+                $referer = array_shift($this->urlSequence);
+                $referer = strtr($referer, array('{$today}' => $today));
+                $credentials = array_shift($this->urlSequence);
+                $credentials = strtr($credentials, array('{$today}' => $today));
                 $fileName = 'CashFlow';
-                $referer ="https://www.mintos.com/en/account-statement/?account_statement_filter[fromDate]=12.09.2017&account_statement_filter[toDate]=12.09.2017&account_statement_filter[maxResults]=20";
+                //$referer ="https://www.mintos.com/en/account-statement/?account_statement_filter[fromDate]={$today}&account_statement_filter[toDate]={$today}&account_statement_filter[maxResults]=20";
                 $this->idForSwitch++;
-                $this->getPfpFileMulticurl(null, $fileName, 'Mintos', 'prueba', $credentialsFile,$referer);
+                $this->getPfpFileMulticurl($url, $credentials, $referer, $fileName);
                 break;
             case 7:
                 $this->idForSwitch++;
