c8d3afb38d93c3a6f9ee06233ae35c1835d07949
Antonio Ibáñez <antonio@winvestify.com>
2017-09-27 14:02:03 +0200

    2017/09/27 | Antonio-Dashboard | Creating collectAmortizationDataClient

--- Console/Command/CollectAmortizationDataClientShell.php
+++ Console/Command/CollectAmortizationDataClientShell.php
@@ -44,7 +44,38 @@ class CollectAmortizationDataClientShell extends AppShell {
         
         $this->Queue = ClassRegistry::init('Queue');
         
-        $resultQueue[] = $this->Queue->getNextFromQueue(FIFO);
+        $resultQueue[] = $this->Queue->getUsersByStatus(FIFO, DATA_EXTRACTED, null, 1);
+        
+        if (empty($resultQueue)) {  // Nothing in the queue
+            echo "empty queue<br>";
+            echo __FILE__ . " " . __FUNCTION__ . " " . __LINE__ . "<br>";
+            exit;
+        }
+
+        //$this->Investor = ClassRegistry::init('Investor');
+        $this->Linkedaccount = ClassRegistry::init('Linkedaccount');
+        $linkedaccountsResults = [];
+        foreach ($resultQueue as $result) {
+            /*$resultInvestor = $this->Investor->find("first", array('conditions' =>
+                array('Investor.investor_identity' => $result['Queue']['queue_userReference']),
+                'fields' => 'id',
+                'recursive' => -1,
+            ));
+            $investorId = $resultInvestor['Investor']['id'];*/
+            $queueInfo = $result['Queue']['queue_info'];
+            $queueInfo = json_decode($queueInfo, true);
+            foreach ($queueInfo['loanIds'] as $key => $loanId) {
+                $linkAccountId[] = $key; 
+            }
+            
+            $filterConditions = array('id' => $linkAccountId);
+            $linkedaccountsResults[] = $this->Linkedaccount->getLinkedaccountDataList($filterConditions);
+            //$linkedaccountsResults[$result['Queue']['queue_userReference']] = $this->Linkedaccount->getLinkedaccountDataList($filterConditions);
+        }
+        
+        $companyTypes = $this->Company->find('list', array(
+            'fields' => array('Company.company_typeAccess')
+        ));
         
     }
     
