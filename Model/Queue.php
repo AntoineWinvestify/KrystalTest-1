e96fae8171ccbbaa107587527b60986723d85937
Antonio Ibáñez <antonio@winvestify.com>
2017-09-27 12:38:35 +0200

    2017/09/27 | Antonio-Dashboard | Fixed function getUsersByStatus on Queue model

--- Model/Queue.php
+++ Model/Queue.php
@@ -187,7 +187,7 @@ public function beforeSave1($options = array()) {
     return true;
 }
 
-    public function getUsersByStatus($queuetype, $status, $userAccess = null, $limit = null) {
+    public function getUsersByStatus($queuetype, $status, $info = null, $limit = null) {
         
         switch ($queuetype) {
             case FIFO:
@@ -201,20 +201,18 @@ public function beforeSave1($options = array()) {
         if (empty($status)) {
             $status =  START_COLLECTING;
         }
+        
+        $conditions = [];
         if (empty($limit)) {
             $limit = 100;
         }
-        if (empty($userAccess)) {
-            $usertype = DAILY_USER;
+        if (!empty($info)) {
+            $conditions["queue_info"] = $info;
         }
-
-        $result = $this->find("all", array("conditions" => array(
-                                                        "queue_type" => $queuetype,
-                                                        "queue_status" => $status,
-                                                        "queue_accessType" => $userAccess),
-                                            "order" => $order,
-                                            "limit" => $limit)
-                            );
+        $conditions["queue_type"] = $queuetype;
+        $conditions["order"] = $order; 
+        $conditions["limit"] = $limit;
+        $result = $this->find("all", array("conditions" => $conditions));
 
         if (empty($result)) {
             return;
