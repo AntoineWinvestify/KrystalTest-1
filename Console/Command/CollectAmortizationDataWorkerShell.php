49c20e977565a234868fae2ac342bcb9ca6b4dc4
Antonio Ibáñez <antonio@winvestify.com>
2017-09-29 12:31:06 +0200

    2017/09/28 | Antonio-Dashboard | Deleted unnecessary lines on collectAmortizationDAta

--- Console/Command/CollectAmortizationDataWorkerShell.php
+++ Console/Command/CollectAmortizationDataWorkerShell.php
@@ -152,9 +152,7 @@ class CollectAmortizationDataWorkerShell extends AppShell {
                if (!empty($error)) {
                    $this->newComp[$info["companyIdForQueue"]]->getError(__LINE__, __FILE__, $info["typeOfRequest"], $error);
                }
-               else {
-                   $this->newComp[$info["companyIdForQueue"]]->saveControlVariables();
-               }
+               
                $this->logoutOnCompany($info["companyIdForQueue"], $str);
                if ($info["typeOfRequest"] == "LOGOUT") {
                    unset($this->tempArray['global']['error']);
