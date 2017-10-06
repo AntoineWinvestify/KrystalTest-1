94d81c05d0683d7b1a043a54ae3d6602b0593839
Antonio Ibáñez <antonio@winvestify.com>
2017-10-06 16:55:26 +0200

    2017/10/06 | Antonio-Dashboard | Updated CollectDataClient with heritage

--- Console/Command/CollectDataClientShell.php
+++ Console/Command/CollectDataClientShell.php
@@ -20,28 +20,17 @@
  * @date
  * @package
  */
-App::uses('Folder', 'Utility');
-App::uses('File', 'Utility');
+App::import('Shell','GearmanClient');
 
 
-class CollectDataClientShell extends AppShell {
+class CollectDataClientShell extends GearmanClientShell {
     protected $GearmanClient;
-    protected $userResult = [];
-    protected $userReference = [];
     protected $newComp = [];
     public $uses = array('Company', 'Urlsequence');
 
     public function startup() {
         $this->GearmanClient = new GearmanClient();
         $this->Applicationerror = ClassRegistry::init('Applicationerror');
-    }
-
-    public function help() {
-        $this->out('Gearman Client as a CakePHP Shell');
-    }
-    
-    public function main() {
-        echo "Nothing to do here";
     }
 
     /**
@@ -143,78 +132,5 @@ class CollectDataClientShell extends AppShell {
         
         
         
-    }
-    
-    /**
-     * Function to catch a fail on a Gearman Worker
-     * @param GearmanTask $task
-     */
-    public function verifyFailTask(GearmanTask $task) {
-        $data = explode(".-;", $task->unique());
-        if (empty($this->userReference[$data[0]])) {
-            $this->userReference[$data[0]] = $data[2];
-        }
-        $this->userResult[$data[0]][$data[1]] = "0";
-        print_r($this->userResult);
-        echo "ID Unique: " . $task->unique() . "\n";
-        echo "Fail: " . $task->data() . GEARMAN_WORK_FAIL . "\n";
-    }
-    
-    /**
-     * 
-     * @param GearmanTask $task
-     */
-    public function verifyExceptionTask (GearmanTask $task) {
-        $data = explode(".-;", $task->unique());
-        if (empty($this->userReference[$data[0]])) {
-            $this->userReference[$data[0]] = $data[2];
-        }
-        $this->userResult[$data[0]][$data[1]] = "0";
-        print_r($this->userResult);
-        echo "ID Unique: " . $task->unique() . "\n";
-        echo "Exception: " . $task->data() . GEARMAN_WORK_EXCEPTION . "\n";
-        //return GEARMAN_WORK_EXCEPTION;
-    }
-    
-    /**
-     * 
-     * @param GearmanTask $task
-     */
-    public function verifyCompleteTask (GearmanTask $task) {
-        $data = explode(".-;", $task->unique());
-        if (empty($this->userReference[$data[0]])) {
-            $this->userReference[$data[0]] = $data[2];
-        }
-        $this->userResult[$data[0]][$data[1]] = $task->data();
-        print_r($this->userResult);
-        print_r($this->userReference);
-        echo "ID Unique: " . $task->unique() . "\n";
-        echo "COMPLETE: " . $task->jobHandle() . ", " . $task->data() . "\n";
-        echo GEARMAN_SUCCESS;
-    }
-    
-    public function consolidationResult($userResult) {
-        $statusProcess = true;
-        foreach ($userResult as $key => $result) {
-            if (!$result) {
-                $statusProcess = false;
-                break;
-            }
-        }
-        return $statusProcess;
-    }
-    
-    public function deleteFolderByDate($key, $date) {
-        $configPath = Configure::read('files');
-        $partialPath = $configPath['investorPath'];
-        $path = $this->userReference[$key] . DS . $date;
-        print_r($this->userReference);
-        $path = $partialPath . DS . $path;
-        $folder = new Folder($path);
-        $delete = false;
-        if (!is_null($folder->path)) {
-            $delete = $folder->delete();
-        }
-        return $delete;
     }
 }
