d1c5a8016498955426fdb75f86e4aee9f5af4082
Antonio Ibáñez <antonio@winvestify.com>
2017-10-10 13:33:18 +0200

    2017/10/10 | Antonio-Dashboard-antonio | Commented curl_timeout error on collectAmortizationDataWorker

--- Console/Command/CollectAmortizationDataWorkerShell.php
+++ Console/Command/CollectAmortizationDataWorkerShell.php
@@ -127,31 +127,31 @@ class CollectAmortizationDataWorkerShell extends GearmanWorkerShell {
                 $this->tempArray[$info["companyIdForQueue"]] = $this->newComp[$info["companyIdForQueue"]]->collectAmortizationTablesParallel($str);
             }
 
-           if (!empty($error) && $error->getCode() == CURL_ERROR_TIMEOUT && $this->newComp[$info["companyIdForQueue"]]->getTries() == 0) {
-               $this->logoutOnCompany($info["companyIdForQueue"], $str);
-               $this->newComp[$info["companyIdForQueue"]]->setIdForSwitch(0); //Set the id for the switch of the function company
-               $this->newComp[$info["companyIdForQueue"]]->setUrlSequence($this->newComp[$info]->getUrlSequenceBackup());  // provide all URLs for this sequence
-               $this->newComp[$info["companyIdForQueue"]]->setTries(1);
-               $this->newComp[$info["companyIdForQueue"]]->deleteCookiesFile();
-               $this->newComp[$info["companyIdForQueue"]]->generateCookiesFile();
-               $this->newComp[$info["companyIdForQueue"]]->collectAmortizationTablesParallel();
-           } 
-           else if ($info["typeOfRequest"] == "LOGOUT") {
-               echo "LOGOUT FINISHED <br>";
-               $this->newComp[$info["companyIdForQueue"]]->deleteCookiesFile();
-           } 
-           else if ((!empty($this->tempArray[$info["companyIdForQueue"]]) || (!empty($error)) && $info["typeOfRequest"] != "LOGOUT")) {
-               if (!empty($error)) {
-                   $this->newComp[$info["companyIdForQueue"]]->getError(__LINE__, __FILE__, $info["typeOfRequest"], $error);
-               }
-               else {
-                   $this->newComp[$info["companyIdForQueue"]]->saveAmortizationTable();
-               }
-               $this->logoutOnCompany($info["companyIdForQueue"], $str);
-               if ($info["typeOfRequest"] == "LOGOUT") {
-                   unset($this->tempArray['global']['error']);
-               }
-           }
+            /*if (!empty($error) && $error->getCode() == CURL_ERROR_TIMEOUT && $this->newComp[$info["companyIdForQueue"]]->getTries() == 0) {
+                $this->logoutOnCompany($info["companyIdForQueue"], $str);
+                $this->newComp[$info["companyIdForQueue"]]->setIdForSwitch(0); //Set the id for the switch of the function company
+                $this->newComp[$info["companyIdForQueue"]]->setUrlSequence($this->newComp[$info]->getUrlSequenceBackup());  // provide all URLs for this sequence
+                $this->newComp[$info["companyIdForQueue"]]->setTries(1);
+                $this->newComp[$info["companyIdForQueue"]]->deleteCookiesFile();
+                $this->newComp[$info["companyIdForQueue"]]->generateCookiesFile();
+                $this->newComp[$info["companyIdForQueue"]]->collectAmortizationTablesParallel();
+            } */
+            if ($info["typeOfRequest"] == "LOGOUT") {
+                echo "LOGOUT FINISHED <br>";
+                $this->newComp[$info["companyIdForQueue"]]->deleteCookiesFile();
+            } 
+            else if ((!empty($this->tempArray[$info["companyIdForQueue"]]) || (!empty($error)) && $info["typeOfRequest"] != "LOGOUT")) {
+                if (!empty($error)) {
+                    $this->newComp[$info["companyIdForQueue"]]->getError(__LINE__, __FILE__, $info["typeOfRequest"], $error);
+                }
+                else {
+                    $this->newComp[$info["companyIdForQueue"]]->saveAmortizationTable();
+                }
+                $this->logoutOnCompany($info["companyIdForQueue"], $str);
+                if ($info["typeOfRequest"] == "LOGOUT") {
+                    unset($this->tempArray['global']['error']);
+                }
+            }
        });
 
        //This is the queue. It is working until there are requests
