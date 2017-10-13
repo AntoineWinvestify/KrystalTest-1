1fcc41801843afb99eef925d824212ca92613786
Antonio Ibáñez <antonio@winvestify.com>
2017-10-13 15:37:48 +0200

    2017/10/13 | Antonio-Dashboard-antonio | Creating functions for consolidationWorker

--- Console/Command/ConsolidationWorkerShell.php
+++ Console/Command/ConsolidationWorkerShell.php
@@ -16,8 +16,8 @@
  *
  *
  * @author 
- * @version
- * @date
+ * @version 0.1
+ * @date 2017-10-13
  * @package
  */
 
@@ -29,5 +29,52 @@ App::import('Shell','GearmanWorker');
  * @author antoiba
  */
 class ConsolidationWorkerShell extends GearmanWorkerShell {
-    //put your code here
+   
+    /**
+     * Function main that init when start the shell class
+     */
+    public function main() {
+        $this->GearmanWorker->addServers('127.0.0.1');
+        $this->GearmanWorker->addFunction('consolidation', array($this, 'consolidateUserData'));
+        echo __FUNCTION__ . " " . __LINE__ . ": " . "Starting GEARMAN_FLOW4 to listen to data from its Client\n";
+        while( $this->GearmanWorker->work() );
+    }
+    
+    /**
+     * Function to initiate the process to save the files of a company
+     * @param object $job It is the object of Gearmanjob that contains
+     * The $job->workload() function read the input data as sent by the Gearman client
+     * This is json_encoded data with the following structure:
+     *      $data["companies"]                  array It contains all the linkedaccount information
+     *      $data["queue_userReference"]        string It is the user reference
+     *      $data["queue_id"]                   integer It is the queue id
+     * @return json Json containing all the status collect and errors by link account id
+     */
+    public function consolidateUserData($job) {
+         $data = json_decode($job->workload(), true);
+        $this->job = $job;
+        $this->Applicationerror = ClassRegistry::init('Applicationerror');
+        print_r($data);
+        $this->queueCurls = new \cURL\RequestsQueue;
+        //If we use setQueueCurls in every class of the companies to set this queueCurls it will be the same?
+        $index = 0;
+        $i = 0;
+        foreach ($data["companies"] as $linkedaccount) {
+            unset($newComp);
+            $index++;
+            echo "<br>******** Executing the loop **********<br>";
+            $companyId = $linkedaccount['Linkedaccount']['company_id'];
+            echo "companyId = $companyId <br>";
+            $companyConditions = array('Company.id' => $companyId);
+            $result = $this->Company->getCompanyDataList($companyConditions);
+            $newComp = $this->companyClass($result[$companyId]['company_codeFile']); // create a new instance of class zank, comunitae, etc.
+            $newComp->defineConfigParms($result[$companyId]);  // Is this really needed??
+            $newComp->setQueueId($data["queue_id"]);
+            $newComp->setCompanyName($result[$companyId]['company_codeFile']);
+            $newComp->setUserReference($data["queue_userReference"]);
+            $newComp->setLinkAccountId($linkedaccount['Linkedaccount']['id']);
+        }
+    }
+    
+    
 }
