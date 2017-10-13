906b281d723b32641615427742328fb0edd57993
Antonio Ibáñez <antonio@winvestify.com>
2017-10-13 12:53:28 +0200

    2017/10/13 | Antonio-Dashboard-antonio | Created Consolidation (flow4) client

---
+++ Console/Command/ConsolidationClientShell.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * +----------------------------------------------------------------------------+
+ * | Copyright (C) 2017, http://www.winvestify.com                   	  	|
+ * +----------------------------------------------------------------------------+
+ * | This file is free software; you can redistribute it and/or modify 		|
+ * | it under the terms of the GNU General Public License as published by  	|
+ * | the Free Software Foundation; either version 2 of the License, or 		|
+ * | (at your option) any later version.                                      	|
+ * | This file is distributed in the hope that it will be useful   		|
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of    		|
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               |
+ * | GNU General Public License for more details.        			|
+ * +----------------------------------------------------------------------------+
+ *
+ *
+ * @author 
+ * @version
+ * @date
+ * @package
+ */
+
+App::import('Shell','GearmanClient');
+
+/**
+ * Description of ConsolidationClientShell
+ *
+ * @author antoiba
+ */
+class ConsolidationClientShell extends GearmanClientShell {
+    //put your code here
+}
