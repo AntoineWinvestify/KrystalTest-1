03883d265a051a250b4b2fc18eba458f267452c2
Antonio Ibáñez <antonio@winvestify.com>
2017-10-13 15:12:33 +0200

    2017/10/13 | Antonio-Dashboard-antonio | Added functions to ConsolidationClient

--- Console/Command/ConsolidationClientShell.php
+++ Console/Command/ConsolidationClientShell.php
@@ -16,8 +16,8 @@
  *
  *
  * @author 
- * @version
- * @date
+ * @version 0.1
+ * @date 2017-10-13
  * @package
  */
 
@@ -29,5 +29,92 @@ App::import('Shell','GearmanClient');
  * @author antoiba
  */
 class ConsolidationClientShell extends GearmanClientShell {
-    //put your code here
+    
+    /**
+     * Function to init the process to recollect all the user investment data
+     *  @param integer $this->args[0]|$queueStatus It is the status we need to use on the search on DB
+     *  @param integer $this->args[1]|$queueTypeAccess It is the access type the user used to get the data
+     */
+    public function initClient() {
+        
+        $inActivityCounter = 0;
+        $this->flowName = "GEARMAN_FLOW0";
+        $this->GearmanClient->addServers();
+        $this->GearmanClient->setExceptionCallback(array($this, 'verifyExceptionTask'));
+
+        $this->GearmanClient->setFailCallback(array($this, 'verifyFailTask'));
+        $this->GearmanClient->setCompleteCallback(array($this, 'verifyCompleteTask'));
+        //$resultQueue = $this->Queue->getUsersByStatus(FIFO, $queueStatus, $queueAccessType);
+        //$resultQueue[] = $this->Queue->getNextFromQueue(FIFO);
+        
+        $inActivityCounter++;                                           // Gearman client 
+        $jobsInParallel = Configure::read('dashboard2JobsInParallel');
+        $this->Investor = ClassRegistry::init('Investor');
+        $this->Linkedaccount = ClassRegistry::init('Linkedaccount');
+        $this->date = date("Ymd");
+        $numberOfIteration = 0;
+        while ($numberOfIteration == 0){
+            $resultQueue  = $this->checkJobs(WIN_QUEUE_STATUS_AMORTIZATION_TABLE_EXTRACTED, $jobsInParallel);
+            print_r($resultQueue);
+            if (!empty($resultQueue)) {
+                $linkedaccountsResults = [];
+                foreach ($resultQueue as $result) {
+                    $queueInfo = json_decode($result['Queue']['queue_info'], true);
+                    $this->queueInfo[$result['Queue']['id']] = $queueInfo;
+                    $resultInvestor = $this->Investor->find("first", array('conditions' =>
+                        array('Investor.investor_identity' => $result['Queue']['queue_userReference']),
+                        'fields' => 'id',
+                        'recursive' => -1,
+                    ));
+                    print_r($resultInvestor);
+                    $investorId = $resultInvestor['Investor']['id'];
+                    $filterConditions = array(  'investor_id' => $investorId,
+                                            );
+                    $linkedaccountsResults[] = $this->Linkedaccount->getLinkedaccountDataList($filterConditions);
+                    echo "linkAccount \n";
+                    print_r($linkedaccountsResults);
+                    //$linkedaccountsResults[$result['Queue']['queue_userReference']] = $this->Linkedaccount->getLinkedaccountDataList($filterConditions);
+                }
+                $userLinkedaccounts = [];
+                foreach ($linkedaccountsResults as $key => $linkedaccountResult) {
+                    //In this case $key is the number of the linkaccount inside the array 0,1,2,3
+                    $i = 0;
+                    foreach ($linkedaccountResult as $linkedaccount) {
+                        $userLinkedaccounts[$key][$i] = $linkedaccount;
+                        //We need to save all the accounts id in case that a Gearman Worker fails,in order to delete all the folders
+                        $this->userLinkaccountIds[$resultQueue[$key]['Queue']['id']][$i] = $linkedaccount['Linkedaccount']['id'];
+                        $i++;
+                    }
+                }
+                
+                //$key is the number of the internal id of the array (0,1,2)
+                //$key2 is type of access to company (multicurl, casper, etc)
+                foreach ($userLinkedaccounts as $key => $userLinkedaccount) {
+                    $data["companies"] = $userLinkedaccount;
+                    $data["queue_userReference"] = $resultQueue[$key]['Queue']['queue_userReference'];
+                    $data["queue_id"] = $resultQueue[$key]['Queue']['id'];
+                    print_r($data["companies"]);
+                    echo "\n";
+                    echo "userReference " . $data["queue_userReference"];
+                    echo "\n";
+                    echo "queueId " . $data["queue_id"];
+                    echo "\n";
+                    echo json_encode($data);
+                    echo "\n";
+                    echo $key2;
+                    echo "\n aquiiiiiiiiiiiiiii";
+                    $this->GearmanClient->addTask('consolidation', json_encode($data), null, $data["queue_id"] . ".-;" . "consolidation" . ".-;" . $resultQueue[$key]['Queue']['queue_userReference']);
+                }
+            }
+            else {
+                $inActivityCounter++;
+                echo __METHOD__ . " " . __LINE__ . " Nothing in queue, so sleeping \n";                
+                sleep (4); 
+            }
+            if ($inActivityCounter > MAX_INACTIVITY) {              // system has dealt with ALL request for tonight, so exit "forever"
+                echo __METHOD__ . " " . __LINE__ . "Maximum Waiting time expired, so EXIT \n";                  
+                exit;
+            }
+        }
+    }
 }
