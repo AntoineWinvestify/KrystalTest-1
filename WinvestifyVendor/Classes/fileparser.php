ebd8c0fa0596dbac6db670afdb2977cb5c036758
eduardo <eduardo@Frodo>
2017-10-27 12:32:58 +0200

    2017-10-27 | Eduardo | Dashboard | fileparser currency fix

--- WinvestifyVendor/Classes/fileparser.php
+++ WinvestifyVendor/Classes/fileparser.php
@@ -802,7 +802,7 @@ print_r($this->config);
      */
     private function getCurrency($loanCurrency) {
 
-        $filter = array(".", ",", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        $filter = array(".", ",", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ");
         $currencySymbol = str_replace($filter, "", $loanCurrency);
 
         foreach ($this->currencies as $currencyIndex => $currency) {
