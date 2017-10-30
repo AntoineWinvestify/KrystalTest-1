e8204a8af5eea5ff7832ac4f003f7d93a32eecb7
Antonio Ibáñez <antonio@winvestify.com>
2017-10-30 08:53:57 +0100

    2017-10-30 | Antonio-Dashboard-antonio | Moved amortizationtables to save only companies that passed verifyStatus function in ParseAmortizatonDataClient

--- Console/Command/ParseAmortizationDataClientShell.php
+++ Console/Command/ParseAmortizationDataClientShell.php
@@ -117,8 +117,9 @@ class ParseAmortizationDataClientShell extends GearmanClientShell {
                 if (Configure::read('debug')) {
                     $this->out(__FUNCTION__ . " " . __LINE__ . ": " . "Result received from Worker\n");
                 }
-                $this->saveAmortizationtablesToDB();
+                
                 $this->verifyStatus(WIN_QUEUE_STATUS_AMORTIZATION_TABLE_EXTRACTED, "Data succcessfully downloaded", WIN_QUEUE_STATUS_DATA_EXTRACTED, WIN_QUEUE_STATUS_UNRECOVERED_ERROR_AMORTIZATION_TABLE);
+                $this->saveAmortizationtablesToDB();
                 unset($pendingJobs);
                 $numberOfIteration++;
             }
