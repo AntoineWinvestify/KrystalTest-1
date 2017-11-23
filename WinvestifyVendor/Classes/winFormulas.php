8268ed88ab308912cec6b646921e8b303efa3941
Antonio Ibáñez <antonio@winvestify.com>
2017-11-23 14:16:53 +0100

    2017-11-23 | Antonio-Dashboard-antonio | Created winFormulas to save winvestify Formulas

---
+++ WinvestifyVendor/Classes/winFormulas.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * +----------------------------------------------------------------------------+
+ * | Copyright (C) 2017, http://www.winvestify.com                   	  	|
+ * +----------------------------------------------------------------------------+
+ * | This file is free software; you can redistribute it and/or modify 		|
+ * | it under the terms of the GNU General Public License as published by  	|
+ * | the Free Software Foundation; either version 2 of the License, or 		|
+ * | (at your option) any later version.                                      	|
+ * | This file is distributed in the hope that it will be useful   		|
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of    		|
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               |
+ * | GNU General Public License for more details.        			|
+ * +----------------------------------------------------------------------------+
+ *
+ *
+ * @author 
+ * @version 0.1
+ * @date
+ * @package
+ */
+
+/**
+ * Class to keep Winvestify formulas to calculate Yield, outstanding
+ *
+ */
+class winFormulas {
+    
+    protected $formula_A = [
+        "A" => [
+            [
+                "type" => "paymenttotal_interestGrossIncome",
+                "operation" => "add",
+                "days" => "1",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_interestIncomeBuyback",
+                "operation" => "add",
+                "days" => "1",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_delayedInterestPayment",
+                "operation" => "add",
+                "days" => "1",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_delayedInterestIncomeBuyback",
+                "operation" => "add",
+                "days" => "1",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_loanCompensation",
+                "operation" => "add",
+                "days" => "1",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_interestGrossIncome",
+                "operation" => "add",
+                "days" => "367",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_interestIncomeBuyback",
+                "operation" => "add",
+                "days" => "367",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_delayedInterestPayment",
+                "operation" => "add",
+                "days" => "367",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_delayedInterestIncomeBuyback",
+                "operation" => "add",
+                "days" => "367",
+                "intervals" => "exclusive"
+            ],
+            [
+                "type" => "paymenttotal_loanCompensation",
+                "operation" => "add",
+                "days" => "367",
+                "intervals" => "exclusive"
+            ],
+        ]
+    ];
+    
+    
+}
