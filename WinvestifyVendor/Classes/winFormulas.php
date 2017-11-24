5e3e89d5c2c88725abcc06c0c0f344ee617d28d8
Antonio Ibáñez <antonio@winvestify.com>
2017-11-24 13:45:35 +0100

    2017-11-23 | Antonio-Dashboard-antonio | Adding configParams in winFormulas

--- WinvestifyVendor/Classes/winFormulas.php
+++ WinvestifyVendor/Classes/winFormulas.php
@@ -25,70 +25,70 @@
  * Class to keep Winvestify formulas to calculate Yield, outstanding
  *
  */
-class winFormulas {
+class WinFormulas {
     
     protected $variablesFormula_A = [
         "A" => [
             [
-                "type" => "paymenttotal_interestGrossIncome",
+                "type" => "userinvestmentdata_totalGrossIncome",
                 "operation" => "add",
+                "dayInit" => "1",
+                "dayFinish" => "367",
+                "intervals" => "inclusive"
+            ],
+            [
+                "type" => "globalcashflowdata_platformTotalCost",
+                "operation" => "substract",
+                "dayInit" => "1",
+                "dayFinish" => "367",
+                "intervals" => "inclusive"
+            ]
+        ],
+        "B" => [
+            [
+                "type" => "userinvestmentdata_outstandingPrincipal",
+                //"operation" => "substract",
+                "dayInit" => "1",
+                "dayFinish" => "367",
+                "intervals" => "inclusive"
+            ],
+            /*[
+                "type" => "userinvestmentdata_outstandingPrincipal",
+                //"operation" => "substract",
+                "days" => "367",
+                "intervals" => "exclusive"
+            ],*/
+            /*[
+                "type" => "userinvestmentdata_partialPrincipalRepayment",
+                "operation" => "substract",
                 "days" => "1",
                 "intervals" => "exclusive"
             ],
             [
-                "type" => "paymenttotal_interestIncomeBuyback",
-                "operation" => "add",
+                "type" => "userinvestmentdata_capitalRepayment",
+                "operation" => "substract",
                 "days" => "1",
                 "intervals" => "exclusive"
             ],
             [
-                "type" => "paymenttotal_delayedInterestPayment",
-                "operation" => "add",
+                "type" => "userinvestmentdata_capitalRepayment",
+                "operation" => "substract",
                 "days" => "1",
                 "intervals" => "exclusive"
             ],
             [
-                "type" => "paymenttotal_delayedInterestIncomeBuyback",
-                "operation" => "add",
+                "type" => "userinvestmentdata_capitalRepayment",
+                "operation" => "substract",
                 "days" => "1",
                 "intervals" => "exclusive"
             ],
             [
-                "type" => "paymenttotal_loanCompensation",
-                "operation" => "add",
+                "type" => "userinvestmentdata_capitalRepayment",
+                "operation" => "substract",
                 "days" => "1",
                 "intervals" => "exclusive"
-            ],
-            [
-                "type" => "paymenttotal_interestGrossIncome",
-                "operation" => "add",
-                "days" => "367",
-                "intervals" => "exclusive"
-            ],
-            [
-                "type" => "paymenttotal_interestIncomeBuyback",
-                "operation" => "add",
-                "days" => "367",
-                "intervals" => "exclusive"
-            ],
-            [
-                "type" => "paymenttotal_delayedInterestPayment",
-                "operation" => "add",
-                "days" => "367",
-                "intervals" => "exclusive"
-            ],
-            [
-                "type" => "paymenttotal_delayedInterestIncomeBuyback",
-                "operation" => "add",
-                "days" => "367",
-                "intervals" => "exclusive"
-            ],
-            [
-                "type" => "paymenttotal_loanCompensation",
-                "operation" => "add",
-                "days" => "367",
-                "intervals" => "exclusive"
-            ],
+            ],*/
+            
         ]
     ];
     
@@ -96,4 +96,69 @@ class winFormulas {
         ["A", "B", "substract"],
         [""]
     ];
+    
+    protected $variablesFormula_B;
+    
+    public function doOperationByType($inputA, $inputB, $type) {
+        
+        switch ($type) {
+            case "add":
+                $result = $this->addTwoValues($inputA, $inputB);
+                break;
+            case "substract":
+                $result = $this->subtractTwoValues($inputA, $inputB);
+                break;
+            case "divide":
+                $result = $this->divideTwoValues($inputA, $inputB);
+                break;
+            case "multiply":
+                $result = $this->multiplyTwoValues($inputA, $inputB);
+                break;
+            case "pow":
+                $result = $this->powTwoValues($inputA, $inputB);
+                break;
+        }
+        return $result;
+    }
+    
+    public function addTwoValues($inputA, $inputB) {
+        return bcadd($inputA, $inputB, 2);
+    }
+    
+    public function subtractTwoValues($inputA, $inputB) {
+        return bcsub($inputA, $inputB, 2);
+    } 
+    
+    public function divideTwoValues($inputA, $inputB) {
+        return bcmul($inputA, $inputB, 2);
+    } 
+    
+    public function multiplyTwoValues($inputA, $inputB) {
+        return bcdiv($inputA, $inputB, 2);
+    } 
+    
+    public function powTwoValues($inputA, $inputB) {
+        return bcpow($inputA, $inputB, 2);
+    }
+    
+    public function getFormulaParams($type) {
+        
+        switch($type) {
+            case "formula_A":
+                return $this->variablesFormula_A;
+            case "formula_B":
+                return $this->variablesFormula_B;
+        }
+    }
+    
+    public function getFormula($type) {
+        
+        switch($type) {
+            case "formula_A":
+                return $this->configFormula_A;
+            case "formula_B":
+                return $this->configFormula_B;
+        }
+    }
+    
 }
