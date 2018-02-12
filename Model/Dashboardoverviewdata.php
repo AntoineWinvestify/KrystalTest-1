b4b444c17a57f97523c40c098a30b1d7bd4a632e
Antonio Ibáñez <antonio@winvestify.com>
2018-02-12 16:50:04 +0100

    2018-02-12 | Antonio-Dashboard | Created model Dashboardoverviewdata

---
+++ Model/Dashboardoverviewdata.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * +----------------------------------------------------------------------------+
+ * | Copyright (C) 2017, http://www.winvestify.com                   	  	|
+ * +----------------------------------------------------------------------------+
+ * | This file is free software; you can redistribute it and/or modify 		|
+ * | it under the terms of the GNU General Public License as published by  	|
+ * | the Free Software Foundation; either version 2 of the License, or 		|
+ * | (at your option) any later version.                                      	|
+ * | This file is distributed in the hope that it will be useful   		|
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of    		|
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               |
+ * | GNU General Public License for more details.        			|
+ * +----------------------------------------------------------------------------+
+ *
+ *
+* @author 
+* @version 0.1
+* @date 2012-02-12
+* @package
+*
+
+
+2017-10-18		version 0.1
+initial version
+
+/**
+ * Description of Dashboardoverviewdata
+ *
+ */
+class Dashboardoverviewdata {
+    
+    var $name = 'Dashboardoverviewdata';
+    var $useTable = "dashboardoverviewdatas";
+    
+    /**
+    *	Apparently can contain any type field which is used in a field. It does NOT necessarily
+    *	have to map to a existing field in the database. Very useful for automatic checks
+    *	provided by framework
+    */
+    var $validate = array(
+
+    );
+
+
+    
+    public $belongsTo = array(
+        'Investor' => array(
+            'className' => 'Investor',
+            'foreignKey' =>  'investor_id'
+        )
+    );
+    
+}
