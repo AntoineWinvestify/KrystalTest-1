4df2768200f4d2d1ddecc3bd0b1a7c95c82cd23a
eduardo <eduardo@Frodo>
2018-02-19 11:38:44 +0100

    2018-02-19 | Eduardo Ibáñez | Maintenance | queue 2 header

--- Console/Command/Queue2Shell.php
+++ Console/Command/Queue2Shell.php
@@ -1,5 +1,30 @@
 <?php
 
+/**
+ * +-----------------------------------------------------------------------------+
+ * | Copyright (C) 2017, http://www.winvestify.com                   	  	|
+ * +-----------------------------------------------------------------------------+
+ * | This file is free software; you can redistribute it and/or modify 		|
+ * | it under the terms of the GNU General Public License as published by  	|
+ * | the Free Software Foundation; either version 2 of the License, or 		|
+ * | (at your option) any later version.                                      	|
+ * | This file is distributed in the hope that it will be useful   		|
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of    		|
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                |
+ * | GNU General Public License for more details.        			|
+ * +-----------------------------------------------------------------------------+
+ *
+ *
+ * @author 
+ * @version 0.1
+ * @date 2018-02-13
+ * @package
+ *
+ *
+ *  Shell for cron methods of queues2
+ *
+ */
+
 require_once(ROOT . DS . 'app' . DS . 'Vendor' . DS . 'autoload.php');
 App::uses('CakeEvent', 'Event');
 App::uses('CakeTime', 'Utility');
