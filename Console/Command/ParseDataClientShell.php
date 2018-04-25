d36696702fac91ce0cce18101dd20339b4088949
antoine <antoine@Gandalf>
2018-04-25 16:47:42 +0200

    2018-04-23 | Antoine | test

--- Console/Command/ParseDataClientShell.php
+++ Console/Command/ParseDataClientShell.php
@@ -62,7 +62,7 @@ class ParseDataClientShell extends GearmanClientShell {
 
 // Only used for defining a stable testbed definition
     public function resetTestEnvironment() {
-        return;
+  //      return;
         echo "Deleting Investment\n";
         $this->Investment->deleteAll(array('Investment.id >' => 0), false);
 
